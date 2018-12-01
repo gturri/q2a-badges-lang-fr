7c0748ec0e192716c950d48fb0ab25bc1d2d4a97
Guillaume Turri <guillaume.turri@gmail.com>
2018-12-01 10:09:38 +0100

    First commit, first translations

---
+++ README.md
@@ -0,0 +1,9 @@
+French translations for the plugin [q2a-badges](https://github.com/NoahY/q2a-badges) of the software [question2answer](https://www.question2answer.org/).
+
+Installation
+============
+(Procedure tested with Question2Answer version 1.8.0)
+
+* Install `question2answer` and the plugin `q2a-badges`
+* Download the `qa-badge-lang-fr.php` file of this repo and copy it to the `qa-plugin/q2a-badges` directory of your website
+* Don't forget to select the French lang for your website from the admin control panel
---
+++ qa-badge-lang-fr.php
@@ -0,0 +1,255 @@
+<?php
+	return array(
+
+		
+		'nice_question'=>'Bonne Question',
+		'good_question'=>'Super  Question',
+		'great_question'=>'Excellente Question',
+
+		'notable_question'=>'Question Notable',
+		'popular_question'=>'Question Populaire',
+		'famous_question'=>'Question Célèbre',
+
+		'nice_answer'=>'Bonne Réponse',
+		'good_answer'=>'Super Réponse',
+		'great_answer'=>'Excellente Réponse',
+
+		'nice_answer_old'=>'Retour',
+		'good_answer_old'=>'Renouveau',
+		'great_answer_old'=>'Resurrection',
+
+
+		'gifted'=>'Doué',
+		'wise'=>'Sage',
+		'enlightened'=>'Éclairé',
+
+		'grateful'=>'Reconnaissant',
+		'respectful'=>'Respectueux',
+		'reverential'=>'Révérentieux',
+						
+		'liked'=>'Aimé',
+		'loved'=>'Adoré',
+		'revered'=>'Vénéré',
+	
+		'asker'=>'Curieux',
+		'questioner'=>'Interrogateur',
+		'inquisitor'=>'Inquisiteur',
+
+		'answerer'=>'Partageur',
+		'lecturer'=>'Professeur',
+		'preacher'=>'Prédicateur',
+
+		'commenter'=>'Commenteur',
+		'commentator'=>'Annoteur',
+		'annotator'=>'Exégète',
+					
+		'voter'=>'Voteur',
+		'avid_voter'=>'Voteur Assidu',
+		'devoted_voter'=>'Voteur Dévoué',
+
+		'editor'=>'Éditeur',
+		'copy_editor'=>'Éditeur Chevronné',
+		'senior_editor'=>'Éditeur Senior',
+		
+		'watchdog'=>'Veilleur',
+		'bloodhound'=>'Limier',
+		'pitbull'=>'Pitbull',
+
+		'reader'=>'Lecteur',
+		'avid_reader'=>'Lecteur Chevronné',
+		'devoted_reader'=>'Lecteur Senior',
+		
+
+		'dedicated'=>'Dédié',
+		'devoted'=>'Devoué',
+		'zealous'=>'Zélé',
+
+
+		'visitor'=>'Visiteur',
+		'trouper'=>'Troupier',
+		'veteran'=>'Vétéran',
+
+		'regular'=>'Regulier',
+		'old_timer'=>'Habitué',
+		'ancestor'=>'Ancien',
+		
+		
+		'100_club'=>'Club des 100',
+		'1000_club'=>'Club des 1.000',
+		'10000_club'=>'Club des 10.000',
+		
+		'medalist'=>'Médaillé',
+		'champion'=>'Champion',
+		'olympian'=>'Olympien',
+
+		'verified'=>'Véritable Humain',
+		'profiler'=>'Autobiographe',
+		'avatar'=>'Photogenique',
+		
+	
+
+		'nice_question_desc'=>'Question ayant reçue +# votes',
+		'good_question_desc'=>'Question ayant reçue +# votes',
+		'great_question_desc'=>'Question ayant reçue +# votes',
+
+		'notable_question_desc'=>'Question ayant reçue # ^view^vues',
+		'popular_question_desc'=>'Question ayant reçue # ^view^vues',
+		'famous_question_desc'=>'Question ayant reçue # ^view^vues',
+		
+		'nice_answer_desc'=>'Réponse ayant reçue +# votes',
+		'good_answer_desc'=>'Réponse ayant reçue +# votes',
+		'great_answer_desc'=>'Réponse ayant reçue +# votes',
+
+		'nice_answer_old_desc'=>'Obtenu le badge $nice_answer en réponse à une question vieille de plus de # ^day^jours',
+		'good_answer_old_desc'=>'Obtenu le badge $good_answer en réponse à une question vieille de plus de # ^day^jours',
+		'great_answer_old_desc'=>'Obtenu le badge $great_answer en résponse à une question vieille de plus de # ^day^days jour',
+		
+		'gifted_desc'=>'# ^answer^réponses sélectionnées en meilleure réponses',
+		'wise_desc'=>'# ^answer^réponses sélectionnées en meilleure réponses',
+		'enlightened_desc'=>'# ^answer^réponses sélectionnées en meilleure réponses',
+
+		'grateful_desc'=>'Sélectionné # ^answer^réponses en tant que meilleure réponse',
+		'respectful_desc'=>'Sélectionné # ^answer^réponses en tant que meilleure réponse',
+		'reverential_desc'=>'Sélectionné # ^answer^réponses en tant que meilleure réponse',
+
+		'liked_desc'=>'Reçu # un total de ^upvote^votes',
+		'loved_desc'=>'Reçu # un total de ^upvote^votes',
+		'revered_desc'=>'Reçu # un total de ^upvote^votes',
+	
+
+		'asker_desc'=>'Posé # ^question^questions',
+		'questioner_desc'=>'Posé # ^question^questions',
+		'inquisitor_desc'=>'Posé # ^question^questions',
+
+		'answerer_desc'=>'Posté # ^answer^réponses',
+		'lecturer_desc'=>'Posté # ^answer^réponses',
+		'preacher_desc'=>'Posté # ^answer^réponses',
+
+		'commenter_desc'=>'Posté # ^comment^commentaires',
+		'commentator_desc'=>'Posté # ^comment^commentaires',
+		'annotator_desc'=>'Posté # ^comment^commentaires',
+		
+				
+		'voter_desc'=>'voté # ^time^fois',
+		'avid_voter_desc'=>'voté # ^time^fois',
+		'devoted_voter_desc'=>'voté # ^time^fois',
+
+		'editor_desc'=>'Effectué un total de # ^edit^éditions',
+		'copy_editor_desc'=>'Effectué un total de # ^edit^éditions',
+		'senior_editor_desc'=>'Effectué un total de # ^edit^éditions',
+		
+		'watchdog_desc'=>'Marqué # ^post^posts commes inappropriés',
+		'bloodhound_desc'=>'Marqué # ^post^posts commes inappropriés',
+		'pitbull_desc'=>'Marqué # ^post^posts commes inappropriés',
+
+		'reader_desc'=>'Lu un total de # ^question^questions',
+		'avid_reader_desc'=>'Lu un total de # ^question^questions',
+		'devoted_reader_desc'=>'Lu un total de # ^question^questions',
+		
+		
+		'dedicated_desc'=>'Est venu chaque jour durant # ^day^jours consécutifs',
+		'devoted_desc'=>'Est venu chaque jour durant # ^day^jours consécutifs',
+		'zealous_desc'=>'Est venu chaque jour durant # ^day^jours consécutifs',
+
+		'visitor_desc'=>'A visité le site un total de # ^day^jours',
+		'trouper_desc'=>'A visité le site un total de # ^day^jours',
+		'veteran_desc'=>'A visité le site un total de # ^day^jours',
+
+		'regular_desc'=>'Première visite il y a plus de # ^day^jours',
+		'old_timer_desc'=>'Première visite il y a plus de # ^day^jours',
+		'ancestor_desc'=>'Première visite il y a plus de # ^day^jours',
+
+		'100_club_desc'=>'Reçu un total de # ^point^points',
+		'1000_club_desc'=>'Reçu un total de # ^point^points',
+		'10000_club_desc'=>'Reçu un total de # ^point^points',
+
+		'medalist_desc'=>'Received total of # ^badge^badges',
+		'champion_desc'=>'Received total of # ^badge^badges',
+		'olympian_desc'=>'Received total of # ^badge^badges',
+
+		'verified_desc'=>'A vérifié son adresse email',
+		'profiler_desc'=>'A rempli tous les champs du profil',
+		'avatar_desc'=>'A uploadé une image en avatar',
+
+
+		
+		'bronze'=>'Bronze',
+		'silver'=>'Argent',
+		'gold'=>'Or',
+
+		'bronze_desc'=>'Les badges de bronze sont accordés pour une participation de base. Ils sont relativement facile à obtenir si vous participez.',
+		'silver_desc'=>'Les badges d\'argent sont accordés pour une participation prolongé. Ils représentent un accomplissement et sont un signe de réelle participation.',
+		'gold_desc'=>'Les badges d\or récompensent les participations extraordinaires. Seule une participation à long terme permet de les obtenir. Ils sont un signe de contribution vraiment précieuse !',
+		
+		'badge_admin'=>'Badge Admin',
+		'badges'=>'Badges',
+		'badge_list_title'=>'Liste des badges',
+		
+		'badge_count_click'=>'Cliquer pour voir les sources',
+		
+		'badge_list_pre'=>'Ci-dessous figure la liste de tous les badges disponibles sur ce site :',
+		'awarded'=>'attribution(s)',
+		'awarded_total'=>'attribution(s) en tout',
+		'badges_total'=>'badges en tout',
+		
+		'badge_notify'=>'Félicitation : vous avez gagné un badge ! ',
+		'badge_notify_multi_plural'=>'et # autres badges !',
+		'badge_notify_multi_singular'=>'et 1 autre badge !',
+		'badge_notify_profile_pre'=>'Consultez ',
+		'badge_notify_profile'=>'votre profile',
+		
+		'badge_names_reset'=>'Réinitialiser les noms des badges.',
+		'badge_values_reset'=>'Réinitialiser les valeurs des badges.',
+		'badge_list_rebuilt'=>'Reconstruire la liste des badges',
+		'badge_admin_saved'=>'Options sauvées.',
+		
+		'badge_admin_activate'=>'Activer les badges',
+		'active_badges'=>'Badges actifs',
+		'badge_award_button'=>'Revérifier les badges accordés',
+		'badge_award_button_desc'=>'Vérifier manuelle les badges accordés',
+		'badge_award_delete_desc'=>'Commencer par supprimer tous les badges de la base de données',
+		'badge_recreate'=>'Recréer les badges',
+		'badge_recreate_desc'=>'Recréer la base de données des badges (en cas de changement du code php)',
+		
+		'badge_admin_click_edit'=>'Cliquer pour éditer le nom du badge',
+		'badge_admin_select_all'=>'Selectionner tout/aucun',
+
+		'badge_reset_css'=>'Réinitialiser le CSS',
+		'badge_reset_values'=>'Réinitialiser les valeurs',
+		'badge_reset_values_desc'=>'Réinitialiser les données des badges saisies ci-dessus',
+		'badge_reset_names'=>'Réinitialiser les noms',
+		'badge_reset_names_desc'=>'Réinitialisater les noms des badges saisis ci-dessus',
+		
+		'notify_time'=>'Durée de la notification',
+		'notify_time_desc'=>'Délai en secondes avant avant de masquer la notification de badge (0 = désactivé)',
+		'badge_trigger_notify'=>'Tester la notification',
+		'badge_trigger_notify_desc'=>'Afficher un exemple de boite de notification',
+		'badge_admin_user_field'=>'Ajouter les badges aux pages de profil',
+		'badge_admin_user_field_no_tab'=>'Afficher les badges sur la page principale du profil (pas dans un onglet de badges)',
+		'badge_show_source_posts'=>'Afficher la liste des posts pour chaque badge sur le profil utilisateur',
+		'badge_show_source_users'=>'Afficher la liste des utilisateurs ayant eu chaque badge sur la page des badges',
+		'badge_admin_user_widget'=>'Afficher les badges sous les points sur le meta des utilisateurs',
+		'badge_admin_user_widget_q_item'=>'Afficher les badges sous les points dans la liste des questions',
+		'badge_admin_loggedin_widget'=>'Afficher les badges sous les points pour l`utilisateur connecté',
+		'badge_show_users_badges'=>'Afficher les badges sous le nom de d\'utilisateur sur la page des utilisateurs',
+
+		'badge_email_notify'=>'Notifier les utilisateurs par email lorsqu\'ils obtiennent un badge',
+		'badge_email_notify_on'=>'Notification par email activées par défaut',
+		'badge_email_subject'=>'Sujet du message',
+		'badge_email_body'=>'Corps du message',
+		'badge_notify_email_me'=>'Notifier des nouveaux badges par email ?',
+		'badge_notified_email_me'=>'Réglages des notifications sauvés.',
+		
+		'badge_widget_title' => 'Badges récents',
+		'widget_list_max' => 'Nombre maximum de badge récents affichés',
+		'widget_date_max' => 'Age maximum des badges récents affichés',
+
+		'save_settings'=>'Sauvegarder',
+		'save_settings_desc'=>'Sauvegarder les réglages ci-dessus'
+	);
+	
+
+/*
+	Omit PHP closing tag to help avoid accidental output
+*/
+
